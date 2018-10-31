c6758383c610e096b7c32b9c7025d225451cbf6f
the00daltonator <the00daltonator@gmail.com>
2018-10-30 23:29:36 -0600

    Arbys fixed

---
+++ Website/mail_sys_example.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<form action="" method="POST">
+	<span>data here:</span><input name="fname" type='text" />
+	<input type="submit" value="Submit form" />
+<form>
+
+<?php
+
+echo fname
+?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
